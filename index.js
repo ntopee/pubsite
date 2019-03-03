1112abf0871eaf0bd76c5495ed4a0318e3113e16
Topi <ntopee@gmail.com>
2019-03-03 14:46:33 +0100

    create express project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('static'));
+app.use(express.static('static', {'index': ['frontpage.html']}));
 var server = app.listen(3000, function () {
+    console.log('listening on http://localhost:3000');
 });
